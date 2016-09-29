7a3914cf1506fc80d432f53b5f14fda5b81169e7
Niti Nrw <niti.patel@flatironschool.com>
2016-09-29 11:55:59 -0400

    Classes Lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,51 @@
 # Make your shoe class here!
+class Shoe
+
+  attr_reader :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    "Nike"
+  end
+
+  def color=(color)
+    @color = "red"
+  end
+
+  # def color
+  #   @color
+  # end
+
+  def size=(size)
+    @size =  9.5
+  end
+
+  # def size
+  #   @size
+  # end
+
+  def material=(material)
+    @material = "suede"
+  end
+
+  # def material
+  #   @material
+  # end
+
+  def condition=(condition)
+    @condition = "tattered"
+  end
+
+  # def condition
+  #   @condition
+  # end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+end
